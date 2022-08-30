9411aea60d2b9e360fe974a2b3e66690c760eeea
broken <spbabo97@naver.com>
2022-08-31 00:45:41 +0900

    [io 테스트]

--- Word-Relay/server.js
+++ Word-Relay/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io');
-
+const socketIo = require('socket.io');
+const io = socketIo(server);
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send('hello');
+app.use(express.static(__dirname + "/public")); // __dirname 을 이용해서 절대경로로 수정
+
+io.on('connection',(socket) => { // 소켓 io을 연결함
+    console.log('소켓 io가 연결되었습니다');
+    socket.on('test',(msg)=>{ // 클라이언트에서 보낸것을 불러옴
+        io.emit('test',msg); // 서버에서 클라이언트로 보내줌
+    });
 });
 
 server.listen(PORT,()=>{
